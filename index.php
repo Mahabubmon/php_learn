c6cc8cc195e550acbdca1bbb37920e3da4cb765d
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-10 11:16:59 +0600

    Strategy

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+$folderPath = 'Strategy_design_pattern/';
+
+
+$files = glob($folderPath . '.php');
+
+
+
+foreach($file as $file){
+    include $file;
+}
+
+$u1 = new User();
+
+$msg = $u1->getMsg();
+
+switch ($msg){
+    case 'email':
+        $objmsg = new SendEmail();
+        break;
+    case 'fax':
+        $objmsg = new SendFax();
+        break;
+    case 'msg':
+        $objmsg = new SendMsg();
+        break;
+    case 'sms':
+        $objmsg = new SendSms();
+        break;
+
+    default:
+    # code...
+        break;
+}
+$objmsg->notification();
+
+?>
