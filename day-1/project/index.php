cb4f852e9b1a8ac0386e439f75a03fa940c13050
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-10 18:09:40 +0600

    html_file_setup_at_project_file

---
+++ day-1/project/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uploading Image File With PHP</title>
+</head>
+<body>
+    <div class="phpcoding">
+        <section class="headeroption">
+            <h2>Uploading Image File with PHP</h2>
+        </section>
+        <section class="mainoption">
+           <div class="myform">
+           <form action="" method ="post" enctype="multipart/form-data">
+            <table>
+                <tr>
+                    <td>Select Image</td>
+                    <td><input type="file" name="image"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="submit" volue="Upload"></td>
+                </tr>
+            </table>
+           </form>
+           </div> 
+        </section>
+        <section class="footeroption">
+            <h2> </h2>
+        </section>
+    </div>
+    
+</body>
+</html>
