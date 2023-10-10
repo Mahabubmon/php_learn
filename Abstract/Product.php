934b72d5721974a868b589dc246e2c2dd0bccbe2
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-10 14:50:13 +0600

    abstract

---
+++ Abstract/Product.php
@@ -0,0 +1,16 @@
+<?php 
+
+    class Product extends Database{
+
+        public function connect(){
+
+        }
+        public function query(){
+
+        }
+        public function insertId(){
+
+        }
+
+    }
+?>
