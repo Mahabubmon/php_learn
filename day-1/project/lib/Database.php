7d44534a92dfe114d83dc48bbcb6a5dab9ab30cd
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-10 18:49:39 +0600

    Databaseconnection

---
+++ day-1/project/lib/Database.php
@@ -0,0 +1,26 @@
+<?php 
+
+    class Database{
+        public $host = DB_HOST;
+        public $user = DB_USER;
+        public $pass = DB_PASS;
+        public $dbname = DB_NAME;
+
+
+        public $link;
+        public $error;
+        function __construct(){
+
+        }
+
+        private function connectDB(){
+            $this->link = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+            if($this->link){
+                $this->error = "Connection fail.".$this->link->connect_error;
+            }
+        }
+
+    }
+
+
+?>
