472ef421e49fded5410afd9de11fd56a026f374b
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-11 10:22:55 +0600

    Showing_Uploaded_image

---
+++ day-2/project/index.php
@@ -0,0 +1,75 @@
+<?php include 'inc/header.php';
+include 'lib/config.php'; 
+include 'lib/Database.php';
+$db  = new Database();
+?>
+
+
+
+           <div class="myform">
+            <?php 
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $permited = array('jpg','jpeg','png','gif');
+                     $file_name = $_FILES['image']['name'];
+                     $file_size = $_FILES['image']['size'];
+                     $file_temp = $_FILES['image']['tmp_name'];
+                     
+                     $div = explode('.',$file_name);
+                     $file_ext = strtolower(end($div));
+                     $unique_image = substr(md5(time()),0,10).'.'.$file_ext;
+                     $uploaded_image = "uploads/".$unique_image;
+                     $folder = "uploads/";
+
+                     if(empty($file_name)){
+                       echo "<span class='error'>Please Select any Image!</span>";
+
+                        }elseif($file_size > 1048579){
+                        echo "<span class='error'>Image Size Should be less than 1MB</span>";
+
+                        }elseif(in_array($file_ext,$permited)== false){
+                            echo "<span class='error'>You Can upload only:-".implode(',', $permited)."</span>";
+
+                        }else{
+                            
+                        
+
+                     move_uploaded_file($file_temp,$uploaded_image);
+                     $query = "INSERT INTO tbl_image(image) VALUES('$uploaded_image')";
+                     $inserted_rows = $db->insert($query);
+                     if($inserted_rows){
+                        echo "<span class='success'>Image Inserted Successfully.</span>";
+                     }else{
+                        echo "<span class='Error'>Image Not Inserted .</span>";
+
+                     }
+                    }
+                }
+            ?>
+           <form action="" method ="post" enctype="multipart/form-data">
+            <table>
+                <tr>
+                    <td>Select Image</td>
+                    <td><input type="file" name="image"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="submit" value="Upload"></td>
+                </tr>
+            </table>
+            <?php 
+                $query ="SELECT *FROM tbl_image order by id desc limit 1";
+                $getImage = $db->select($query);
+                if($getImage){
+                    while($result = $getImage->fetch_assoc()){
+            ?>
+            <img src="<?php echo $result['image'];?>" height="100px" width="200px"/>
+                        
+            <?php
+             
+            }
+                }
+            ?>
+           </form>
+           </div> 
+           <?php include 'inc/footer.php'?>
+        
---
+++ day-2/project/lib/Database.php
@@ -0,0 +1,50 @@
+<?php 
+
+    class Database{
+        public $host = DB_HOST;
+        public $user = DB_USER;
+        public $pass = DB_PASS;
+        public $dbname = DB_NAME;
+
+
+        public $link;
+        public $error;
+        function __construct(){
+            $this->connectDB();
+
+        }
+
+        private function connectDB(){
+            $this->link = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+            if($this->link){
+                $this->error = "Connection fail.".$this->link->connect_error;
+            }
+        }
+        //Insert Data
+        public function insert($data)
+        {
+            $insert_row = $this->link->query($data) or die ($this->link->error.__LINE__);
+            if($insert_row){
+                return $insert_row;
+            }else{
+                return false;
+            }
+
+        }
+        //sELECT Data
+        public function select($data)
+        {
+            $result = $this->link->query($data) or die ($this->link->error.__LINE__);
+            if($result->num_rows > 0){
+                return $result;
+            }else{
+                return false;
+            }
+
+        }
+
+
+    }
+
+
+?>
