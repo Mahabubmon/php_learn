144c51a4d31fd44026cb51797b9842e8316816f2
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-11 16:35:07 +0600

    database_connecting

--- day-2/Blog_page/index.php
+++ day-2/Blog_page/index.php
@@ -1,8 +1,24 @@
+<?php include 'config/config.php'?>
+<?php include 'lib/Database.php'?>
 <?php include 'inc/header.php';?>
 <?php include 'inc/slider.php';?>
 
+<?php 
+
+  $db = new Database();
+
+?>
+
 <div class="contentsection contemplete clear">
   <div class="maincontent clear">
+
+  <?php 
+    $query = "select * from tbl_post";
+    $post =  $db->select($query);
+    if($post){
+
+    
+  ?>
     <div class="samepost clear">
       <h2><a href="">Our post title here</a></h2>
       <h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
@@ -20,40 +36,10 @@
         <a href="post.php">Read More</a>
       </div>
     </div>
-    <div class="samepost clear">
-      <h2><a href="">Our post title here</a></h2>
-      <h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
-      <img src="images/post2.png" alt="post image" />
-      <p>
-        Some text will be go here. Some text will be go here.Some text will be
-        go here.Some text will be go here.Some text will be go here.Some text
-        will be go here.Some text will be go here.Some text will be go here.Some
-        text will be go here.Some text will be go here. Some text will be go
-        here.Some text will be go here.Some text will be go here.Some text will
-        be go here.Some text will be go here.Some text will be go here.Some text
-        will be go here.Some text will be go here.
-      </p>
-      <div class="readmore clear">
-        <a href="post.php">Read More</a>
-      </div>
-    </div>
-    <div class="samepost clear">
-      <h2>Our post title here</h2>
-      <h4>April 10, 2016, 12:30 PM, By <a href="#">Delowar</a></h4>
-      <img src="images/post1.jpg" alt="post image" />
-      <p>
-        Some text will be go here. Some text will be go here.Some text will be
-        go here.Some text will be go here.Some text will be go here.Some text
-        will be go here.Some text will be go here.Some text will be go here.Some
-        text will be go here.Some text will be go here. Some text will be go
-        here.Some text will be go here.Some text will be go here.Some text will
-        be go here.Some text will be go here.Some text will be go here.Some text
-        will be go here.Some text will be go here.
-      </p>
-      <div class="readmore clear">
-        <a href="post.php">Read More</a>
-      </div>
-    </div>
+    <?php 
+    }else{
+      header("Location:404.php");
+    }?>
   </div>
   
 </div>
