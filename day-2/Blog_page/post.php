babbfd4b5d0abf1390d9d9dbcbf011ca15b3addd
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-11 20:21:52 +0600

    post_portion

--- day-2/Blog_page/post.php
+++ day-2/Blog_page/post.php
@@ -14,16 +14,22 @@ if(!isset($_GET['id'])|| $_GET['id' == NULL]){
 			<div class="about">
 				<?php 
 					$query = "select * from tbl_post where id='$id'";
+					$post =  $db->select($query);
+    if($post){
+      while($result = $post->fetch_assoc()){
 				?>
 				<h2>Our post title here</h2>
 				<h4>April 10, 2016, 12:30 PM, By Delowar</h4>
-				<img src="images/post2.png" alt="MyImage"/>
+				<a href="#"><img src="admin/upload/<?php echo $result['image'];?>" alt="post image" /></a>
 				<p>Our psot..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
 				
 				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
 
 				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
-				
+				<?php }}else{
+					header("Location:404.php");
+
+				}?>
 				<div class="relatedpost clear">
 					<h2>Related articles</h2>
 					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
