9e4f9f123814470d630de8aa698ca1e4629cce3a
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-12 15:02:38 +0600

    session_added_and_functionality_configaring

---
+++ day-2/Blog_page/lib/Session.php
@@ -0,0 +1,23 @@
+<?php 
+
+    class Session{
+        // Session:init
+        public static function init(){
+            session_start();
+        }
+
+        public static function set($key, $val){
+            $_SESSION['$key'] = $val;
+        }
+
+        public static function get($key){
+            if (isset($_SESSION['$key'])){
+                return $_SESSION['$key'];
+
+            }else{
+                return false;
+            }
+        }
+    }
+
+?>
