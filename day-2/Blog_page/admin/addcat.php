5c34e77007e96215ac51a761f974b1b688020ef7
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-12 20:20:07 +0600

    Add_portion_some moe modification

--- day-2/Blog_page/admin/addcat.php
+++ day-2/Blog_page/admin/addcat.php
@@ -4,8 +4,23 @@
 
 <div class="box round first grid">
     <h2>Add New Category</h2>
-    <div class="block copyblock"> 
-        <form>
+    <div class="block copyblock">
+        <?php 
+ if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $name = $_POST['name'];
+    $name = mysqli_real_escape_string($db->link,$name);
+    
+    if(empty($name)){
+        echo "<span class = 'error'>Feild must not empty!</span>";
+
+    }else{
+        $query = "INSERT INTO tbl_category(name) VALUES('$name')";
+        $catinsert = $db->insert($query);
+    }
+}			
+        
+        ?> 
+        <form action="" method="post">
         <table class="form">					
             <tr>
                 <td>
