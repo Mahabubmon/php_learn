35503315ac11d6a238ce0044a49688e8b5bee447
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-15 19:52:47 +0600

    some code cleaning

--- day-2/Blog_page/post.php
+++ day-2/Blog_page/post.php
@@ -20,7 +20,7 @@ if(!isset($_GET['id'])|| $_GET['id'] == NULL){
 				?>
 				<h2><?php echo $result['title'];?></h2>
 				<h4><?php echo $fm->formatDate($result['date']);?> By <a href="#"><?php echo $result['author'];?></a></h4>
-				<img src="admin/upload/<?php echo $result['image'];?>" alt="post image" />
+				<img src="admin/<?php echo $result['image'];?>" alt="post image" />
 				<?php echo $result['body'];?>
 				
 				
@@ -35,7 +35,7 @@ if(!isset($_GET['id'])|| $_GET['id'] == NULL){
 					
 					?>
 					<a href="post.php?id=<?php echo $rresult['id'];?>">
-						<img src="admin/upload/<?php echo $rresult['image'];?>" alt="post image"/></a>
+						<img src="admin/<?php echo $rresult['image'];?>" alt="post image"/></a>
 					<?php }}else{
 							echo"No Related Post Availabe";
 						}?>
