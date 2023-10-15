0cd92e21aa5eba715ccd9f3f377deb3150e08b53
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-15 19:03:41 +0600

    some code_added

--- day-2/Blog_page/admin/addpost.php
+++ day-2/Blog_page/admin/addpost.php
@@ -21,7 +21,27 @@
                          $div = explode('.', $file_name);
                          $file_ext = strtolower(end($div));
                          $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
-                         $uploaded_image = "uploads/".$unique_image;
+                         $uploaded_image = "upload/".$unique_image;
+
+                    if($title == "" ||$cat == "" ||$body == "" ||$tags == "" ||$author == "" ||$file_name == ""){
+                        echo "<span class = 'error'>Feild must not empty!</span>";
+                    }elseif ($file_size >1048567) {
+                        echo "<span class='error'>Image Size should be less then 1MB!
+                        </span>";
+                       } elseif (in_array($file_ext, $permited) === false) {
+                        echo "<span class='error'>You can upload only:-"
+                        .implode(', ', $permited)."</span>";
+                       } else{
+                            move_uploaded_file($file_temp, $uploaded_image);
+                            $query = "INSERT INTO tbl_image(image) VALUES('$uploaded_image')";
+                            $inserted_rows = $db->insert($query);
+                            if ($inserted_rows) {
+                                echo "<span class='success'>Image Inserted Successfully.
+                                </span>";
+                            }else {
+                                echo "<span class='error'>Image Not Inserted !</span>";
+                            }
+                         }
                     }
                 ?>
                 <div class="block">               
