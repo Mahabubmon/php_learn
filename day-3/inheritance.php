d25366013f884f5e0af5a4494848d2793d7673d0
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-19 23:27:49 +0600

    inheritrnce

---
+++ day-3/inheritance.php
@@ -0,0 +1,26 @@
+<?php
+class River
+{
+    public $padma;
+    public $meghna;
+
+
+
+    public function __construct()
+    {
+        $this->padma = $padma;
+    }
+}
+
+class Flow extends River
+{
+    public function place()
+    {
+        echo "mawa";
+    }
+}
+
+$identifyRiver = new River();
+
+
+?>
